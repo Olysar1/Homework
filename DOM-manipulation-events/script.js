4fd854a2196a8d8805de97c69cc1339d8bf86aaf
Olysar1 <giorgibarbakadz1@gmail.com>
2022-12-11 15:17:17 +0400

    add latest homework

--- DOM-manipulation-events/script.js
+++ DOM-manipulation-events/script.js
@@ -4,7 +4,19 @@
 const button = document.querySelector("#hider");
 const text = document.querySelector("#text");
 
-button.addEventListener("click", function () {
+button.addEventListener("click", () => {
   console.log("btn clicked \n text hidden");
   text.style.visibility = "hidden";
 });
+
+//Task2:
+const insertedTag = document.createElement("div");
+insertedTag.id = "card";
+insertedTag.innerHTML = "<h2>George</h2> <a href='#'>Go to Profile</a>";
+
+text.insertAdjacentElement("afterend", insertedTag);
+
+//Task3:
+const newDiv = document.querySelector("#card");
+newDiv.style.backgroundColor = "red";
+console.log(newDiv);
